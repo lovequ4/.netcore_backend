a020a5a706728610d5bac246ac5d2c361ad31133
sam <a0983131803@gmail.com>
2023-08-13 09:24:16 +0800

    Create Modles Folder and user.cs 、 producrt.cs

---
+++ Models/Product.cs
@@ -0,0 +1,14 @@
+namespace backend.Models;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public string ProductName { get; set; }
+
+    public string Description { get; set; }
+
+    public int Price { get; set;}
+
+    public int Quantity { get; set; }
+}
