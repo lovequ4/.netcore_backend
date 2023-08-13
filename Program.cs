bf4de05c9bc505e688b4ea56159e0135113ddb42
sam <a0983131803@gmail.com>
2023-08-13 10:01:46 +0800

    Add AppDBContext

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
+using backend.Context;
+using backend.Service;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDBContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("WebApiDatabase"))
+);
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
